76a8ae439c0ae1b7307333ca4360922444b3599f
Kevin McCabe <kmccabe1@email.unc.edu>
2022-11-05 12:44:19 -0400

    Added dice rolling function

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,22 @@
 #!/usr/bin/env node
-// File that runs when package is installed
+// Load dependencies
+import minimist from 'minimist'
+import { roll } from '../lib/roll.js'
+
+// Parse cli args with minimist
+const args = minimist(process.argv.slice(2))
+// Set default values, but update if args given
+let sides = 6
+if (args.sides) {
+	sides = args.sides
+}
+let dice = 2
+if (args.dice) {
+	dice = args.dice
+}
+let rolls = 1
+if (args.rolls) {
+	rolls = args.rolls
+}
+
+console.log(JSON.stringify(roll(sides, dice, rolls)))
