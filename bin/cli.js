6a63a018644f2ff9dfef9deba269023039e81304
Kevin McCabe <kmccabe1@email.unc.edu>
2022-11-05 19:31:22 -0400

    Added error handling

--- bin/cli.js
+++ bin/cli.js
@@ -7,16 +7,23 @@ import { roll } from '../lib/roll.js'
 const args = minimist(process.argv.slice(2))
 // Set default values, but update if args given
 let sides = 6
-if (args.sides) {
+if (args.sides > 0) {
 	sides = args.sides
+}else {
+	console.log("Sides must be positive.")
 }
 let dice = 2
-if (args.dice) {
+if (args.dice > 0) {
 	dice = args.dice
+}else {
+	console.log("Dice must be positive.")
 }
 let rolls = 1
-if (args.rolls) {
+if (args.rolls > 0) {
 	rolls = args.rolls
+}else {
+	console.log("Rolls must be positive.")
 }
 
 console.log(JSON.stringify(roll(sides, dice, rolls)))
+process.exit(0)
